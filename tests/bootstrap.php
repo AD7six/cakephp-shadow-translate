68b0250dd10f67e8aed216de6fcb62f4b70e1622
AD7six <andydawson76@gmail.com>
2014-10-18 09:53:07 +0000

    update tests bootstrap, create composer.json stub

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,17 @@
 <?php
-/**
- * Test suite bootstrap for ShadowTranslate.
- */
-// Customize this to be a relative path for embedded plugins.
-// For standalone plugins, this should point at a CakePHP installation.
-require '/var/www/cakephp.dev/config/bootstrap.php';
+function find_root($root) {
+	do {
+		$lastRoot = $root;
+		$root = dirname($root);
+		if (is_dir($root . '/vendor/cakephp/cakephp')) {
+			return $root;
+		}
+	} while($root !== $lastRoot);
+
+	throw new Exception("Cannot find the root of the application, unable to run tests");
+}
+
+$root = find_root(__FILE__);
+
+chdir($root);
+require $root . '/config/bootstrap.php';
