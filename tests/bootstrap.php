d0675d94bac6b9cf0d01836a11e70b2363fdea06
AD7six <andydawson76@gmail.com>
2014-11-20 11:13:08 +0000

    use re-baked tests bootstrap file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,12 @@
 <?php
-function find_root($root) {
+/**
+ * Test suite bootstrap for <%= $plugin %>.
+ *
+ * This function is used to find the location of CakePHP whether CakePHP
+ * has been installed as a dependency of the plugin, or the plugin is itself
+ * installed as a dependency of an application.
+ */
+$findRoot = function($root) {
 	do {
 		$lastRoot = $root;
 		$root = dirname($root);
@@ -9,9 +16,9 @@ function find_root($root) {
 	} while($root !== $lastRoot);
 
 	throw new Exception("Cannot find the root of the application, unable to run tests");
-}
-
-$root = find_root(__FILE__);
+};
+$root = $findRoot(__FILE__);
+unset($findRoot);
 
 chdir($root);
 require $root . '/config/bootstrap.php';
