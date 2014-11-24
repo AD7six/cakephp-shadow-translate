76827efc8ef719090fe305274342a1625a6f7e16
AD7six <andydawson76@gmail.com>
2014-11-24 15:52:13 +0000

    add equivalent fixtures
    Equivalent to the fixtures used by the core translate behavior test

---
+++ tests/Fixture/AuthorsTranslationsFixture.php
@@ -0,0 +1,32 @@
+<?php
+namespace ShadowTranslate\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * Class AuthorsTranslationsFixture
+ *
+ */
+class AuthorsTranslationsFixture extends TestFixture {
+
+/**
+ * fields property
+ *
+ * @var array
+ */
+	public $fields = [
+		'id' => ['type' => 'integer'],
+		'locale' => ['type' => 'string', 'null' => false],
+		'name' => ['type' => 'string', 'null' => false],
+		'_constraints' => ['primary' => ['type' => 'primary', 'columns' => ['id', 'locale']]]
+	];
+
+/**
+ * records property
+ *
+ * @var array
+ */
+	public $records = [
+		['locale' => 'eng', 'id' => 1, 'name' => 'May-rianoh'],
+	];
+}
